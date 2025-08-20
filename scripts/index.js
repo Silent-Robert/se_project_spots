09a164694afaddc5bf435113304fbbbe8ba54008
Robert Paiz Silent <rob3diamond@gmail.com>
2025-08-20 02:59:40 -0600

    take 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-
 const editProfileModal = document.querySelector("#edit-profile-modal");
-
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
-
 const newPostModal = document.querySelector("#new-post-modal");
-
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
