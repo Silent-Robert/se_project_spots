a4a434e326b5b03bf02c5c5bebc51a3f0a78e134
Robert Paiz Silent <rob3diamond@gmail.com>
2025-08-20 03:32:34 -0600

    fixed intexjs and html

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,17 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-opened");
+  editProfileModal.classList.add("modal__is-open");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  editProfileModal.classList.remove("modal__is-open");
 });
 
-newPostBtnBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal__is-open");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  newPostModal.classList.remove("modal__is-open");
 });
